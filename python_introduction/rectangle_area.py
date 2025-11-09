e5ec96e6ad9ea88635e054557ae2f320e12ba411
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-09 12:00:47 +0000

    Create rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+#Use basic Python arithmetic operations and variable assignments to calculate the area of a rectangle using its length and width.
+#Area of a rectangle is Area = length * width
+length = 10
+width = 5
+area = length * width
+print("The area of the rectangle is:", area)
